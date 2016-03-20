ab9ac1f139c1b8243708857edcc3c269f69cb10d
aninditaayu <aninditaayu12@gmail.com>
2016-03-20 15:55:03 +0700

    Final skripsi

---
+++ skripsi_coba/settings-deploy.py
@@ -0,0 +1,14 @@
+from settings import *
+
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['be-py.com']
+
+STATIC_ROOT='/home/bepycom/skripsi/static/'
+
+STATICFILES_DIRS = (
+    '/home/bepycom/skripsi/static/'.
+)
